ca0bbbe95b48eac00c848b5c60f059a6a01e45ae
David Stanley <davidstanley01@gmail.com>
2014-03-27 12:52:32 -0700

    Initial Commit

---
+++ AuraFacades/Facade.php
@@ -0,0 +1,30 @@
+<?php
+namespace AuraFacades;
+
+use Illuminate\Support\Facades\Facade as AwesomeFacade;
+
+class Facade extends AwesomeFacade
+{
+    protected static $aura;
+
+    public static function registerAliases($aliases = null)
+    {
+        if (!$aliases) {
+            $aliases = array(
+                // aliases go here
+            );
+        }
+
+        foreach ($aliases as $alias => $class) {
+            class_alias($class, $alias);
+        }
+    }
+
+    public static function setFacadeApplication($app)
+    {
+        parent::$app = $app->container;
+        self::$app   = $app->container;
+
+        self::$aura = $app;
+    }
+}
